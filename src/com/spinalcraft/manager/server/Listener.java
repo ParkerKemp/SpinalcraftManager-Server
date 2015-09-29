e39737786784cb2e2890b43b9a5cc8866f8ef52b
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-09-29 14:01:30 -0500

    Send a response

--- src/com/spinalcraft/manager/server/Listener.java
+++ src/com/spinalcraft/manager/server/Listener.java
@@ -1,6 +1,8 @@
 package com.spinalcraft.manager.server;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,6 +16,8 @@ public class Listener implements Runnable {
 			socket = new ServerSocket(9494);
 			while(true){
 				conn = socket.accept();
+				PrintStream printer = new PrintStream(conn.getOutputStream());
+				printer.println("Hello from the server!");
 				System.out.println("Got a connection!");
 			}
 		} catch (IOException e) {
