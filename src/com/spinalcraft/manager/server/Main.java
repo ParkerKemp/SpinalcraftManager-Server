ecf8e58355c020c169d9a035cacb020d5e999438
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-10-26 18:33:46 -0500

    Move service logic into ManagerService

--- src/com/spinalcraft/manager/server/Main.java
+++ src/com/spinalcraft/manager/server/Main.java
@@ -1,19 +1,36 @@
 package com.spinalcraft.manager.server;
 
 import com.spinalcraft.easycrypt.messenger.Messenger;
-import com.spinalcraft.manager.server.communication.Listener;
+import com.spinalcraft.manager.server.communication.Crypt;
 import com.spinalcraft.manager.server.component.RefreshUsernames;
 
 public class Main {
 	public final static String dbName = "Manager";
 	private final static boolean shouldShowDebug = true;
+	
+	private static ManagerService service;
 
 	public static void main(String[] args){
 		Messenger.shouldShowDebug = shouldShowDebug;
 		Database.getInstance().init(dbName);
 		(new Thread(new RefreshUsernames())).start();
-		(new Thread(new Listener())).start();
+//		(new Thread(new Listener())).start();
 		(new Thread(new Notifier())).start();
+		
+		initService();
+		service.serve();
+	}
+	
+	private static void initService(){
+		service = (ManagerService)new ManagerService(Crypt.getInstance())
+				.setIdentity("manager")
+				.setServiceAddress("mc.spinalcraft.com")
+				.setPort(9495);
+		
+		if(!service.init("373pprp6m0r2aqkd765u1suqdo")){
+			System.err.println("Failed to register with authentication server! Exiting...");
+			return;
+		}
 	}
 	
 	public static void debug(String string){
--- src/com/spinalcraft/manager/server/ManagerService.java
+++ src/com/spinalcraft/manager/server/ManagerService.java
@@ -1,16 +1,25 @@
 package com.spinalcraft.manager.server;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.newsclub.net.unix.AFUNIXSocket;
+import org.newsclub.net.unix.AFUNIXSocketAddress;
+
+import com.google.gson.Gson;
 import com.spinalcraft.berberos.service.BerberosService;
+import com.spinalcraft.berberos.service.ServiceAmbassador;
 import com.spinalcraft.easycrypt.EasyCrypt;
 import com.spinalcraft.easycrypt.messenger.MessageReceiver;
 import com.spinalcraft.easycrypt.messenger.MessageSender;
 import com.spinalcraft.manager.server.communication.messenger.Receiver;
 import com.spinalcraft.manager.server.communication.messenger.Sender;
+import com.spinalcraft.manager.server.component.ApplicationManager;
 
 public class ManagerService extends BerberosService{
 
@@ -19,7 +28,95 @@ public class ManagerService extends BerberosService{
 	}
 
 	@Override
-	protected boolean authenticatorCached(String authenticator) {
+	protected void onAuthenticated(ServiceAmbassador ambassador) {
+		if(ambassador == null){
+			System.err.println("Failed to authenticate user.");
+			return;
+		}
+		
+		processRequest(ambassador);
+	}
+	
+	private void processRequest(ServiceAmbassador ambassador){
+		MessageReceiver receiver = ambassador.receiveMessage();
+		if(receiver == null)
+			return;
+		String intent = receiver.getHeader("intent");
+		MessageSender sender = ambassador.getSender();
+		switch(intent){
+		case "applicationList":
+			sendApplicationList(ambassador, receiver);
+			break;
+		case "applicationAnswer":
+			processApplicationAnswerRequest(ambassador, receiver);
+			break;
+		case "message":		
+			String message = receiver.getItem("message");
+			System.out.println("Received Message: " + message);
+			sender = ambassador.getSender();
+			sender.addItem("message", message + " to you as well");
+			ambassador.sendMessage(sender);
+			break;
+		}
+	}
+	
+	private void sendApplicationList(ServiceAmbassador ambassador, MessageReceiver receiver){
+		String filter = receiver.getItem("filter");
+		MessageSender sender = ambassador.getSender();
+		Gson gson = new Gson();
+		String json = gson.toJson(ApplicationManager.getApplications(filter));
+		sender = ambassador.getSender();
+		sender.addHeader("status", "good");
+		sender.addItem("applications", json);
+		ambassador.sendMessage(sender);
+	}
+	
+	private void processApplicationAnswerRequest(ServiceAmbassador ambassador, MessageReceiver receiver){
+		String identity = receiver.getHeader("identity");
+		String uuid = receiver.getItem("uuid");
+		boolean accept = Boolean.parseBoolean(receiver.getItem("accept"));
+		MessageSender sender = ambassador.getSender();
+		Application application = ApplicationManager.completeApplication(uuid, accept, identity);
+		if(application != null){
+			if(accept && whitelistAdd(application.username))
+				notifyApplicant(application.email, application.username);
+			sender.addHeader("status", "good");
+			Gson gson = new Gson();
+			sender.addItem("application", gson.toJson(application));
+			ambassador.sendMessage(sender);
+		}
+		else{
+			sender.addHeader("status", "bad");
+			ambassador.sendMessage(sender);
+		}
+	}
+	
+	private void notifyApplicant(String email, String username){
+		Runtime rt = Runtime.getRuntime();
+		try {
+			rt.exec("/usr/local/sbin/notifyAcceptedApplication " + email + " " + username);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean whitelistAdd(String username){
+		File socketFile = new File("/home/minecraft/server/dev/plugins/Spinalpack/sockets/command.sock");
+		try {
+			AFUNIXSocket socket = AFUNIXSocket.newInstance();
+			socket.connect(new AFUNIXSocketAddress(socketFile));
+			PrintStream printer = new PrintStream(socket.getOutputStream());
+			printer.print("whitelist add " + username);
+			socket.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean authenticatorCached(String authenticator) {
 		String query = "SELECT * FROM authenticators WHERE authenticator = ?";
 		PreparedStatement stmt;
 		try {
@@ -35,7 +132,7 @@ public class ManagerService extends BerberosService{
 	}
 
 	@Override
-	protected boolean cacheAuthenticator(String authenticator) {
+	public boolean cacheAuthenticator(String authenticator) {
 		String query = "INSERT INTO authenticators (authenticator) VALUES (?)";
 		try {
 			PreparedStatement stmt = Database.getInstance().prepareStatement(query);
